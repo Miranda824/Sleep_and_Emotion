789c3d0e730b75300c556ecb6c0e6dbef7839a98
Miranda824 <132531410+Miranda824@users.noreply.github.com>
2024-07-18 16:00:42 +0800

    Update util.py

--- Sleep/util.py
+++ Sleep/util.py
@@ -1,11 +1,5 @@
 import os
 
-# import memory_profiler
-# def show_menory():
-#     usage=int(memory_profiler.memory_usage()[0])
-#     print('menory usage:',usage,'MB')
-#     return usage
-
 def writelog(log,printflag = False):
     f = open('./log','a+')
     f.write(log+'\n')
