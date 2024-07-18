2c1f0603e33e1fe2c874ec56d1b85b0d1f342a93
Miranda824 <132531410+Miranda824@users.noreply.github.com>
2024-07-18 14:36:23 +0800

    Update creatnet.py

--- Sleep/creatnet.py
+++ Sleep/creatnet.py
@@ -12,19 +12,3 @@ def CreatNet(name):
         raise ValueError(f"Invalid network name: {name}")
 
     return net
-
-
-# from models.Deepsleepnet import create_deepsleepnet
-#
-#
-# def CreatNet(name):
-#     if name == 'deepsleepnet':
-#         input_shape = (1, 3188)  # 根据你的数据形状进行修改
-#         model = create_deepsleepnet(input_shape)
-#         return model
-#
-#     return None
-#
-# # 根据你的需要选择模型名称
-# model_name = 'deepsleepnet'
-# model = CreatNet(model_name)
