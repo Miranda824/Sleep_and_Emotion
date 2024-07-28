4ae992168c0d5ab33daa80d9ef156bc8076c49d8
Miranda824 <132531410+Miranda824@users.noreply.github.com>
2024-07-28 13:03:26 +0800

    Update creatnet.py

--- Sleep/creatnet.py
+++ Sleep/creatnet.py
@@ -1,12 +1,8 @@
 from torch import nn
-from models import attNsoft1
-# from models import attNsoft1
-
-
-from models import attNsoft1
+from models import attNsoft
 
 def CreatNet(name):
-    if name == 'attNsoft1':
+    if name == 'attNsoft':
         net = attNsoft1.Multi_Scale_ResNet(inchannel=1, num_classes=5)
     else:
         raise ValueError(f"Invalid network name: {name}")
