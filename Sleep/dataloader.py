169a70bbf49b76d168f10df577e8f5209c7540ea
Miranda824 <132531410+Miranda824@users.noreply.github.com>
2024-12-05 14:16:39 +0800

    Update dataloader.py

--- Sleep/dataloader.py
+++ Sleep/dataloader.py
@@ -1,30 +1,14 @@
 import os
-import time
 import random
 from scipy.signal import resample
-import scipy.io as sio
 import numpy as np
-import h5py
 import mne
 import pyeeg as pe
 from sklearn.preprocessing import normalize
 import transformer
 import tensorflow as tf
 import pandas as pd
-from tensorflow.keras.models import load_model
 
-def postprocess_predictions(predictions):
-    """
-    Round the predicted values to the nearest integer and return as a numpy array.
-    """
-    rounded_predictions = np.round(predictions)
-    return rounded_predictions
-
-def trimdata(data, num):  # Trim the length of data to be a multiple of num
-    return data[:num * int(len(data) / num)]
-
-def reducesample(data, mult):  # Downsample the data by a factor of mult
-    return data[::mult]
 
 # Delete useless labels
 def del_UND(signals, stages):
@@ -37,7 +21,8 @@ def del_UND(signals, stages):
             cnt += 1
     return signals, stages
 
-def connectdata(signal, stage, signals=[], stages=[]):  # Connect new signal and stage data with existing data
+# Connect new signal and stage data with existing data
+def connectdata(signal, stage, signals=[], stages=[]):
     if signals == []:
         signals = signal.copy()
         stages = stage.copy()
@@ -46,62 +31,11 @@ def connectdata(signal, stage, signals=[], stages=[]):  # Connect new signal and
         stages = np.concatenate((stages, stage), axis=0)
     return signals, stages
 
-# Load one subject data from cc2018
-def loaddata_cc2018(filedir, filename, signal_name, BID, filter=True):
-    dirpath = os.path.join(filedir, filename)
-    # Load signal
-    hea_path = os.path.join(dirpath, os.path.basename(dirpath) + '.hea')
-    signal_path = os.path.join(dirpath, os.path.basename(dirpath) + '.mat')
 
-    # Fix path issue
-    hea_path = hea_path.replace('\\', '/')
-    signal_path = signal_path.replace('\\', '/')
-
-    signal_names = []
-    for i, line in enumerate(open(hea_path), 0):
-        if i != 0:
-            line = line.strip()
-            signal_names.append(line.split()[8])
-    mat = sio.loadmat(signal_path)
-    signals = mat['val'][signal_names.index(signal_name)]
-
-    # Load stage
-    stagepath = os.path.join(dirpath, os.path.basename(dirpath) + '-arousal.mat')
-
-    # Fix path issue
-    stagepath = stagepath.replace('\\', '/')
-
-    mat = h5py.File(stagepath, 'r')
-    # N3(S4+S3)->0  N2->1  N1->2  REM->3  W->4  UND->5
-    N3 = mat['data']['sleep_stages']['nonrem3'][0]
-    N2 = mat['data']['sleep_stages']['nonrem2'][0]
-    N1 = mat['data']['sleep_stages']['nonrem1'][0]
-    REM = mat['data']['sleep_stages']['rem'][0]
-    W = mat['data']['sleep_stages']['wake'][0]
-    UND = mat['data']['sleep_stages']['undefined'][0]
-    stages = N3 * 0 + N2 * 1 + N1 * 2 + REM * 3 + W * 4 + UND * 5
-    # Resample
-    signals = reducesample(signals, 2)
-    stages = reducesample(stages, 2)
-    # Trim
-    signals = trimdata(signals, 3000)
-    stages = trimdata(stages, 3000)
-    # 30s per label
-    signals = signals.reshape(-1, 3000)
-    stages = stages[::3000]
-    # Balance individualized differences
-    signals = transformer.Balance_individualized_differences(signals, BID)
-    # Delete UND
-    signals, stages = del_UND(signals, stages)
-
-    return signals.astype(np.float16), stages.astype(np.int16)
-
-# Load one subject data from sleep-edfx
 # Load data from the specified dataset, signal, number, BID, etc., from the specified folder
 # Read the signal data of the specified signal channel in the data file and convert it to numpy array format
 # Read the annotation information in the data file and convert the sleep stages to numeric labels
 # According to the label information and signal data, segment the sleep signal data into corresponding sleep stages and return the segmented sleep signal data and label data
-# Optionally, trim the sleep signal data to retain only the specified time period
 # Balance individualized differences in signal data
 # Return the processed sleep signal data and label data
 def loaddata_sleep_edfx(filedir, filename, signal_name, BID, select_sleep_time, model):
@@ -205,22 +139,8 @@ def loaddata_sleep_edfx(filedir, filename, signal_name, BID, select_sleep_time, 
     # #select sleep time
     if select_sleep_time:
         if 'SC' in f_signal_name:
-            # first_annot = raw_annot[0]
-            # duration_80percent = int(first_annot['duration'] * 0.2)
-            # first_annot['duration'] = duration_80percent
-            # raw_annot[0] = first_annot
-
             signals = signals[np.clip(int(raw_annot[0]['duration'])//30-60,0,9999999):int(raw_annot[-2]['onset'])//30+60]
             stages = stages[np.clip(int(raw_annot[0]['duration'])//30-60,0,9999999):int(raw_annot[-2]['onset'])//30+60]
-            # signals = signals[np.clip(int(raw_annot[0]['duration'])//30-60,0,9999999):int(raw_annot[-2]['onset'])//30+60]
-            # stages = stages[np.clip(int(raw_annot[0]['duration'])//30-60,0,9999999):int(raw_annot[-2]['onset'])//30+60]
-    # if select_sleep_time:
-    #     if 'SC' in f_signal_name:
-    #         duration = int(raw_annot[0]['duration'])
-    #         onset = int(raw_annot[0]['onset'])
-    #         remove_end = int(onset) + duration - (500 * 100)
-    #         signals = signals[np.clip(int(remove_end) // 30 - 60, 0, 9999999):int(raw_annot[-2]['onset']) // 30 + 60]
-    #         stages = stages[np.clip(int(remove_end) //30-60,0,9999999):int(raw_annot[-2]['onset'])//30+60]
 
     signals,stages = del_UND(signals, stages)
     # print('shape:',signals.shape,stages.shape)
@@ -245,7 +165,7 @@ def loaddata_sleep_edfx(filedir, filename, signal_name, BID, select_sleep_time, 
     return signals.astype(np.float16),stages.astype(np.int16)
 
 #load all data in datasets
-def loaddataset(filedir,dataset_name,signal_name,num,BID,select_sleep_time,shuffle = True,model=None):  # num, the amount of data to load
+def loaddataset(filedir,dataset_name,signal_name,num,BID,select_sleep_time,shuffle = True,model=None):
 
     print('load dataset, please wait...')
     filenames = os.listdir(filedir)
@@ -254,20 +174,11 @@ def loaddataset(filedir,dataset_name,signal_name,num,BID,select_sleep_time,shuff
     signals=[]
     stages=[]
     if dataset_name in ['sleep-edfx','sleep-edfx-8']:
-        # if num > 197:
-        #      num = 197
         if dataset_name == 'sleep-edfx-8':
             filenames = ['SC4002E0-PSG.edf','SC4012E0-PSG.edf','SC4102E0-PSG.edf','SC4112E0-PSG.edf',
-            'ST7022J0-PSG.edf','ST7052J0-PSG.edf','ST7121J0-PSG.edf','ST7132J0-PSG.edf']
-        cnt = 0
+                         'ST7022J0-PSG.edf','ST7052J0-PSG.edf','ST7121J0-PSG.edf','ST7132J0-PSG.edf']
         for filename in filenames:
             if 'PSG' in filename:
                 signal,stage = loaddata_sleep_edfx(filedir,filename,signal_name,BID,select_sleep_time,model)
                 signals,stages = connectdata(signal,stage,signals,stages)
-                # cnt += 1
-                # if cnt == num:
-                #     break
     return signals,stages
-
-
-
