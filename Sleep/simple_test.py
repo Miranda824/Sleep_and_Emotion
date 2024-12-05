b4b83752e55dcabdb7245159deaae4f77bf064f7
Miranda824 <132531410+Miranda824@users.noreply.github.com>
2024-12-05 15:47:01 +0800

    Update simple_test.py

--- Sleep/simple_test.py
+++ Sleep/simple_test.py
@@ -1,11 +1,9 @@
 import numpy as np
 import torch
 import matplotlib.pyplot as plt
-
 import transformer
 from options import Options
 from creatnet import CreatNet
-
 from sklearn.manifold import TSNE
 
 opt = Options().getparse()
@@ -17,11 +15,9 @@ if not opt.no_cuda:
     net.cuda()
 if not opt.no_cudnn:
     import torch.backends.cudnn as cudnn
-
     cudnn.benchmark = True
 
 # load prtrained_model
-# net.load_state_dict(torch.load('./checkpoints/' + opt.model_name + '.pth'))
 net.load_state_dict(torch.load('./transcheckpoint/' + 'attNsoft' + '.pth'))
 net.eval()
 
@@ -35,19 +31,9 @@ def runmodel(eeg):
     pred_stage = pred.data.cpu().numpy()
 
     out = out.data.cpu().numpy()
-    # print(pred_stage[0],'---pred+_out--------------------------', out)
     return pred_stage[0], out
 
 
-'''
-you can change your input data here.
-but the data needs meet the following conditions: 
-1.fs = 100Hz
-2.collect by uv
-3.type   numpydata  signals:np.float16  stages:np.int16
-4.shape             signals:[?,3000]   stages:[?]
-'''
-#载入数据集，对数据进行处理并进行了打印输出
 eegdata = np.load('./datasets/simple_test/signals.npy')
 true_stages = np.load('./datasets/simple_test/stages.npy')
 print('shape of eegdata:', eegdata.shape)
@@ -57,19 +43,17 @@ true_stages = true_stages[:500]
 print('shape of eegdata:', eegdata.shape)
 print('shape of true_stage:', true_stages.shape)
 
-# Normalize归一化
+# Normalize
 eegdata = transformer.Balance_individualized_differences(eegdata, '5_95_th')
 
-# run pretrained model对每个信号进行模型预测并记录预测结果和输出结果
+# run pretrained model
 pred_stages = []
 out_tsne = []
 for i in range(len(eegdata)):
     pred_stages.append(runmodel(eegdata[i])[0])
-
     out_tsne.append(runmodel(eegdata[i])[1])
-
 pred_stages = np.array(pred_stages)
-#这是一个绘制嵌入图的函数
+
 def plot_embedding(data, label, title):
     x_min, x_max = np.min(data, 0), np.max(data, 0)
     data = (data - x_min) / (x_max - x_min)
@@ -79,7 +63,6 @@ def plot_embedding(data, label, title):
     ax = plt.subplot(111)
 
     for i in range(data.shape[0]):
-        # plt.scatter(data[i, 0], data[i, 1], c=dict2[label[i]])
         plt.text(data[i, 0], data[i, 1], str(dict1[label[i]]),
                  color=dict2[label[i]],
                  fontdict={'weight': 'bold', 'size': 3})
@@ -88,13 +71,11 @@ def plot_embedding(data, label, title):
     plt.title(title)
     return fig
 
-
 out_tsne = np.squeeze(np.array(out_tsne), 1)
 tsne = TSNE(n_components=2, init='pca', perplexity=30, random_state=0)
 X = tsne.fit_transform(eegdata)
 X1 = tsne.fit_transform(out_tsne)
 fig = plot_embedding(X, true_stages, 'digits')
-
 fig1 = plot_embedding(X1, true_stages, 't-SNE embedding of the digits')
 
 plt.show()
