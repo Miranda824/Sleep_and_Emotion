e7199b8c7f684f40352eacdf98ec6d56aca19044
Miranda824 <132531410+Miranda824@users.noreply.github.com>
2024-12-05 15:48:28 +0800

    Update rocNpr.py

--- Sleep/rocNpr.py
+++ Sleep/rocNpr.py
@@ -1,10 +1,7 @@
-import numpy as np
 import matplotlib.pyplot as plt
-from itertools import cycle
 from sklearn.metrics import roc_curve, auc
 from sklearn.metrics import precision_recall_curve
 from sklearn.preprocessing import label_binarize
-from scipy import interp
 
 
 def pr_plot(y_score, y_stage):
@@ -13,10 +10,8 @@ def pr_plot(y_score, y_stage):
     pr_auc=dict()
     y_stage = label_binarize(y_stage, classes=[0, 1, 2, 3, 4])
 
-
     pre["micro"],rec["micro"], _ = precision_recall_curve(y_stage.ravel(), y_score.ravel())
     pr_auc["micro"] = auc(rec["micro"],pre["micro"])
-
 
     lw = 2
     plt.figure()
@@ -33,19 +28,14 @@ def pr_plot(y_score, y_stage):
     plt.title('P_R')
     plt.legend(loc="lower right")
     plt.savefig('./pr.png')
-    #   plt.pause(0.05)
     plt.close()
 
 
 def roc_plot(y_score, y_stage):
-    # 绘制ROC曲线
     fpr = dict()
     tpr = dict()
     roc_auc = dict()
     y_stage = label_binarize(y_stage, classes=[0, 1, 2, 3, 4])
-    # for i in range(5):
-    #     fpr[i], tpr[i], _ = roc_curve(y_stage[:, i], y_score[:, i])
-    #     roc_auc[i] = auc(fpr[i], tpr[i])
 
     fpr["micro"], tpr["micro"], _ = roc_curve(y_stage.ravel(), y_score.ravel())
     roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
@@ -65,5 +55,4 @@ def roc_plot(y_score, y_stage):
     plt.title('micro-average roc')
     plt.legend(loc="lower right")
     plt.savefig('./roc.png')
-#   plt.pause(0.05)
     plt.close()
